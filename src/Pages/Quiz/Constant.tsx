e5e3b7da78f29f8f97a3cc12eacec1b38097425a
Mariam Martirosyan <76228722+MariamMartirosyann@users.noreply.github.com>
2022-10-11 23:35:20 +0400

    ...

--- src/Pages/Quiz/Constant.tsx
+++ src/Pages/Quiz/Constant.tsx
@@ -1,7 +1,8 @@
 export interface IQuestion {
 title:string;
 variants:string[]  ,
-correct:number
+correct:number,
+incorrect:number[]
 }
 
 
@@ -9,22 +10,26 @@ export const questions:IQuestion[]=[
     {
         title:'What is the capital of Armenia?',
         variants:["Yerevan", "New York", "Alabama","Burkinafaso"],
-        correct:0
+        correct:0,
+        incorrect:[1,2,3]
     },
     {
         title:'What is the capital of UK?',
         variants:["Moscow", "New York", "Washingtom","London"],
-        correct:3
+        correct:3,
+        incorrect:[0,1,2,]
     },
     {
         title:'What is the capital of China?',
         variants:["Tokio", "Pekin", "Shanhie","Alyaska"],
-        correct:1
+        correct:1,
+        incorrect:[0,2,3]
     },
     {
         title:'What is the capital of Russion Federation?',
         variants:["Orinburg", "Peterburg", "Moscow","Kiev"],
-        correct:2
+        correct:2,
+        incorrect:[0,1,3]
     },
 
 
--- src/Pages/Quiz/Quiz.tsx
+++ src/Pages/Quiz/Quiz.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { questions } from "./Constant";
 import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store';
@@ -7,67 +7,82 @@ import "./style.css"
 
 
 const Quiz: React.FC = () => {
+    const name = useSelector((state: RootState) => state.nameInfo.name)
 
 
     const [score, setScore] = useState<number>(0);
     const [step, setStep] = useState<number>(0);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState<any>(0)
+    const [currentAnswerIndex, setCurrentAnswerIndex] = useState<number>()
+
     const question = questions[step];
-
-
-
-
-    const name = useSelector((state: RootState) => state.nameInfo.name)
-
-    //const isCorrectAnswer= variants[correct]
-    //const isWrongAnswer= currentAnswer === answerText && currentAnswer !==correctAnswer;
-
-    // const correctAnswerClass = isCorrectAnswer ? "correct": "";
-    //const wrongAnswerClass = isWrongAnswer ? "incorrect": "";
-    //const disabledClass = currentAnswer ? "incorrect": "";
-
+    console.log("currentAnswerIndex", currentAnswerIndex)
 
 
     const onClickVariant = (index: number) => {
+
+        const nextQuestion = step + 1
+        setCurrentQuestionIndex(nextQuestion)
+
         if (step < questions.length) {
             setStep(step + 1)
             setScore(score + 100);
+            setCurrentAnswerIndex(index)
+             
+
         }
 
     }
- const handleStartAgain =(event:any)=>{
- setStep(0)
- setScore(0)
- }
+    console.log(currentQuestionIndex, "currentQuestionIndex")
+
+    const handleShuffle = (array: any[]) => {
+        [...array].sort(() => Math.random() - 0.5);
+    }
+
+    const handleStartAgain = (event: any) => {
+        setStep(0)
+        setScore(0)
+    }
+
+    const variantsArray = question.variants;
+    const correctAnswerIndex = question.correct;
+    const inCorrectAnswerIndex = question.incorrect
+    const correctAnwser = variantsArray[correctAnswerIndex];
+    const inCorrectAnswer = variantsArray.filter(i => i !== correctAnwser)
+ 
+    console.log("correctAnswer",correctAnwser)
+    console.log("inCorrectAnswer",inCorrectAnswer)
+
+    ///[correctAnwser,...inCorrectAnswer]
+    // useEffect(() => {
+
+    //    {setStep(question &&
+    //     handleShuffle([
+    //         correctAnwser,
+    //         ...inCorrectAnswer,
+    //     ]
+    //     );)} 
+    // }, [ question]);
+
 
     return (
         <div className='main'>
             {step !== questions.length ? (<><div className="welcome"> {questions[step].title}</div>
-            <div className='qustions'>
-                {question.variants.map((text, index) => <button key={text} onClick={() => onClickVariant(index)} className="question">{text}</button>)}
-            </div>
-            <div className='info'>
-                <div>Score: {score}</div>
-                <div>Question {step + 1}/{questions.length}</div>
+                <div className='qustions'>
+                    {question.variants.map((text, index) =>
+                        <button key={text} onClick={() => onClickVariant(index)} className={"question"}>{text}
+                        </button>)}
+                </div>
+                <div className='info'>
+                    <div>Score: {score}</div>
+                    <div>Question {step + 1}/{questions.length}</div>
 
-            </div></>):(<> <div className="welcome">Quiz is over </div>
+                </div></>) : (
+                <> <div className="welcome">Quiz is over </div>
 
-          <div className='finalInfo'> {name} your score is {score}, from {questions.length} questions</div>
-          <div className='playAgain' onClick={handleStartAgain}> Play again</div></>)  }
+                    <div className='finalInfo'> {name} your score is {score}, from {questions.length} questions</div>
+                    <div className='playAgain' onClick={handleStartAgain}> Play again</div></>)}
 
-{/*          Questions
-            <div className="welcome"> {questions[step].title}</div>
-            <div className='qustions'>
-                {question.variants.map((text, index) => <button key={text} onClick={() => onClickVariant(index)} className="question">{text}</button>)}
-            </div>
-            <div className='info'>
-                <div>Score: {score}</div>
-                <div>Question {step + 1}/{questions.length}</div>
-
-            </div> */}
-       {/* <div className="welcome">Quiz is over </div>
-
-          <div className='finalInfo'> {name} your score is {score}, from {questions.length} questions</div>
-          <div className='playAgain'> Play again</div> */}
         </div>
     )
 }
@@ -75,11 +90,3 @@ const Quiz: React.FC = () => {
 
 export default Quiz
 
-
-/*{questions[currentQuestion].variants.map((variant) => (
-
-    <button  key={variant} 
-    
-    >
-        {variant}</button>
-))}*/
